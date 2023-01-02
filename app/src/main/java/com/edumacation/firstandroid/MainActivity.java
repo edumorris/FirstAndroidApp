e9de65be0949f0bbf264e8598ffe4b8fa5f4703e
edumorris <eduuwachira@gmail.com>
2023-01-02 12:21:58 +0300

    Initial commit. Button event.

---
+++ app/src/main/java/com/edumacation/firstandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.edumacation.firstandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toggle(View v) {
+        v.setEnabled(false);
+    }
+}
