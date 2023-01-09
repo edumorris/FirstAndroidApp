3bfdd86bde0e097d3deb68427ba2222ed4ef294e
edumorris <eduuwachira@gmail.com>
2023-01-09 12:02:29 +0300

    Toast, Icon change, Changing text of an element

--- app/src/main/java/com/edumacation/firstandroid/MainActivity.java
+++ app/src/main/java/com/edumacation/firstandroid/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
 
         btn.setText("Disabled"); // Changes the text of the button to the one that is defined.
         Log.d("Success", "Button disabled, Text changed");
+    }
+
+    public void handleText(View V) {
+        EditText t = findViewById(R.id.sourceText);
+        String input = t.getText().toString();
+
+        ((TextView) findViewById(R.id.textView2)).setText(input);
+
+        Log.d("info", input);
+
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show(); // Display toast message
     }
 }
